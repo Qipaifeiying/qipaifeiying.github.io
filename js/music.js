5800493b328efbd49f2b18a69b4d5d094d8ed9b2
qipaifeiying <1752669709@qq.com>
2024-10-03 20:09:15 +0800

    test qq id

--- js/music.js
+++ js/music.js
@@ -13,9 +13,9 @@ GitHub：https://github.com/imsyy/home
 // let type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
 // let id = "7452421335"; //封面 ID / 单曲 ID / 歌单 ID
 
-let server = "kugou"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
-let type = "mixsong"; //song: 单曲; playlist: 歌单; album: 唱片
-let id = "nme5vaa"; //封面 ID / 单曲 ID / 歌单 ID
+let server = "tencent"; //netease: 网易云音乐; tencent: QQ音乐; kugou: 酷狗音乐; xiami: 虾米; kuwo: 酷我
+let type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
+let id = "9338366108"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
     url: "https://api.wuenci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id,
